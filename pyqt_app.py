316fa26a1f64e3abc7af8dd6a35dc2c19f9cadd5
Dimitri Dias Moreira <taikamya@outlook.com>
2022-08-30 14:12:20 +0200

    first commit

--- pyqt_app.py
+++ pyqt_app.py
@@ -4,3 +4,20 @@
 Sample PyQt6 GUI to be reused and modified in other projects
 '''
 
+from PyQt6.QtWidgets import QApplication, QWidget
+import sys
+
+
+app = QApplication(sys.argv)
+
+
+if __name__=="__main__":
+
+    # Create the window
+    window = QWidget()
+
+    # Show the window
+    window.show()
+
+    # Start the loop
+    app.exec()
